e19515c51f4af516ada46f3750c62708a5839bec
mlf <micafinn@gmail.com>
2016-12-26 18:29:35 +0200

    Test for method to log out if one browser gets logged out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,7 +51,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "password shoudl be present (nonblank)" do
+  test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
@@ -60,6 +60,10 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with a nil digest" do
+    assert_not @user.authenticated?('')
+  end 
 end
 #     test "email validation should reject invalid addresses" do
 #       invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
