66307a29930e3baa72e352c40d25b3cecdf066f8
mlf <micafinn@gmail.com>
2016-12-26 18:19:49 +0200

    Test for Only logging out if logged in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,8 +18,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information followed by logout" do
     get login_path
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'password'} }
+    post login_path, params: { session: { email:    @user.email,
+                                          password:   'password'} }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -30,6 +30,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    #Simulate a user clicking lohout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
