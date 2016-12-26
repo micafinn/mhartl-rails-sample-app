716c4042cc64786a4668571b805dcca6417894c9
mlf <micafinn@gmail.com>
2016-12-26 14:01:39 +0200

    Add forget method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,3 @@
-# class User < ApplicationRecord
-#   before_save { email.downcase!}
-#   validates :name, presence: true, length: { maximum: 50 }
-#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#   validates :email, presence: true, length: { maximum: 255 },
-#                     format: { with: VALID_EMAIL_REGEX },
-#                     uniqueness: {case_sensitive: false},
-#   has_secure_password
-# end
-#why does the above cause errors? of unexpecte /n type.
-
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
@@ -40,5 +29,10 @@ class User < ApplicationRecord
   #Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  #Forgets user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
